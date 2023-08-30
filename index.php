1b45158da0c7528b7afb37412c1683193ed085b3
rolihi <royerliconahinojosa@gmail.com>
2023-08-29 21:43:02 -0400

    proyecto

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+include 'bd/BD.php';
+
+header('Access-Control-Allow-Origin: *');
+
+if($_SERVER['REQUEST_METHOD']=='GET'){
+    if(isset($_GET['id'])){
+        $query="select * from medico where id=".$_GET['id'];
+        $resultado=metodoGet($query);
+        echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
+    }else{
+        $query="select * from medico";
+        $resultado=metodoGet($query);
+        echo json_encode($resultado->fetchAll()); 
+    }
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='POST'){
+    unset($_POST['METHOD']);
+    $nombre=$_POST['nombre'];
+    $direccion=$_POST['direccion'];
+    $especialidad=$_POST['especialidad'];
+    $sueldo=$_POST['sueldo'];
+    $query="insert into medico(nombre, direccion, especialidad, sueldo) values ('$nombre', '$direccion', '$especialidad', '$sueldo')";
+    $queryAutoIncrement="select MAX(id) as id from medico";
+    $resultado=metodoPost($query, $queryAutoIncrement);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='PUT'){
+    unset($_POST['METHOD']);
+    $id=$_GET['id'];
+    $nombre=$_POST['nombre'];
+    $direccion=$_POST['direccion'];
+    $especialidad=$_POST['especialidad'];
+    $sueldo=$_POST['sueldo'];
+    $query="UPDATE medico SET nombre='$nombre', direccion='$direccion', especialidad='$especialidad', sueldo='$sueldo' WHERE id='$id'";
+    $resultado=metodoPut($query);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+if($_POST['METHOD']=='DELETE'){
+    unset($_POST['METHOD']);
+    $id=$_GET['id'];
+    $query="DELETE FROM medico WHERE id='$id'";
+    $resultado=metodoDelete($query);
+    echo json_encode($resultado);
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+header("HTTP/1.1 400 Bad Request");
+
+
+?>
